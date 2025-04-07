1aba4a013f8cb82cd9bd72c31f67fbee8c185bd7
Pastel-Gnome <89545079+Pastel-Gnome@users.noreply.github.com>
2025-04-06 23:27:15 -0500

    Update Template Files
    - Previous template was not opening when pulled, a new template project has been created

--- SleepingGod/Intermediate/Build/Win64/x64/SleepingGodEditor/Development/Slate/SharedPCH.Slate.Cpp20.h
+++
@@ -1,3 +0,0 @@
-// PCH for Runtime/Slate/Public/SlateSharedPCH.h
-#include "C:/Users/oulis/Documents/Unreal Projects/SleepingGod/Intermediate/Build/Win64/x64/SleepingGodEditor/Development/Slate/SharedDefinitions.Slate.Cpp20.h"
-#include "Runtime/Slate/Public/SlateSharedPCH.h"
